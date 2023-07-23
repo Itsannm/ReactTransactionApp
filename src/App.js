263fac64aeacf94d2bd26f8c35f3eff0caab43ad
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-07-23 18:21:10 +0300

    feed the components to App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import React, { useState }from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TransactionTable from './TransactionTable';
+import TransactionForm from './TransactionForm';
+import SearchBar from './SearchBar';
 
-function App() {
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  const handleAddTransaction = (newTransaction) => {
+    setTransactions([...transactions, newTransaction]);
+  };
+
+  const handleSearch = (searchTerm) => {
+    // Filter transactions based on the search term
+    const filteredTransactions = transactions.filter((transaction) =>
+      transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setTransactions(filteredTransactions);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bank Transactions App</h1>
+      <TransactionForm onAddTransaction={handleAddTransaction} />
+      <SearchBar onSearch={handleSearch} />
+      <TransactionTable transactions={transactions} />
     </div>
   );
-}
+};
 
 export default App;
